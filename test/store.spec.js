1354bbea20483e08abe26ba1ce6a8b5303e6551f
Dmitri Zagidulin <dzagidulin@gmail.com>
2018-05-27 14:45:29 -0400

    Initial commit

---
+++ test/store.spec.js
@@ -0,0 +1,10 @@
+'use strict'
+
+// const chai = require('chai')
+// const sinon = require('sinon')
+// const { expect } = chai
+// chai.use(require('dirty-chai'))
+// chai.use(require('sinon-chai'))
+
+describe('Store', () => {
+})
