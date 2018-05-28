805383ad35f055915e69b8fa93ed85e779193668
Dmitri Zagidulin <dzagidulin@gmail.com>
2018-05-28 15:16:41 -0400

    Add basic unit test.

--- test/store.spec.js
+++ test/store.spec.js
@@ -1,10 +1,16 @@
 'use strict'
 
-// const chai = require('chai')
+const chai = require('chai')
 // const sinon = require('sinon')
-// const { expect } = chai
-// chai.use(require('dirty-chai'))
+const { expect } = chai
+chai.use(require('dirty-chai'))
 // chai.use(require('sinon-chai'))
 
+const Store = require('../src/store')
+
 describe('Store', () => {
+  it('should exist', () => {
+    const store = new Store({})
+    expect(store).to.exist()
+  })
 })
