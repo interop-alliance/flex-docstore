0f6f2703cfa0232792fdebc91ecde2f6470d20d7
Dmitri Zagidulin <dzagidulin@gmail.com>
2018-05-28 15:16:27 -0400

    Implement Store API.

--- src/store.js
+++ src/store.js
@@ -1,3 +1,65 @@
 'use strict'
 
-module.exports = {}
+const FlexFileStore = require('./backends/files')
+const MockStore = require('./backends/mock')
+
+const DEFAULT_COLLECTION = 'db'
+
+class Store {
+  constructor (options) {
+    this.backend = options.backend
+    this.className = options.className
+    this.collection = options.collection || DEFAULT_COLLECTION
+  }
+
+  static using (type, options) {
+    let backend
+
+    switch (type) {
+      case 'files':
+        backend = new FlexFileStore(options)
+        break
+      case 'mock':
+      default:
+        backend = new MockStore(options)
+        break
+    }
+
+    return new Store({backend, ...options})
+  }
+
+  async get (key) {
+    this.validateKey(key)
+
+    const doc = await this.backend.get(key)
+    if (!this.className) {
+      return doc // resolve with raw JSON
+    }
+
+    return this.className.from(doc || {})
+  }
+
+  async put (key, data) {
+    this.validateKey(key)
+
+    return this.backend.put(key, data)
+  }
+
+  async remove (key) {
+    this.validateKey(key)
+
+    return this.backend.remove(key)
+  }
+
+  async allDocs () {
+    return this.backend.allDocs()
+  }
+
+  validateKey (key) {
+    if (!key) {
+      throw new Error('Key cannot be empty')
+    }
+  }
+}
+
+module.exports = Store
